9dfc2143fc1523f4bbbe359f5a06df8c8e65a63b
txyge <slastenov2004@mail.ru>
2024-09-21 00:40:50 +0300

    Add files via upload

---
+++ Lab_1/Задание_3.rb
@@ -0,0 +1,13 @@
+puts"3---------------------"
+#Ввод команды для Ruby
+print"Введите команду языка Ruby:"
+Ruby_Command=$stdin.gets.chomp
+#Вывод результата команды для Ruby
+puts"Результатик для Руби:"
+eval(Ruby_Command)
+#Ввод команды для ОС
+print"Введите команду операционной системы:"
+OS_Command=$stdin.gets.chomp
+#Вывод результата команды для ОС
+puts"Результатик для ОС:"
+system(OS_Command)
