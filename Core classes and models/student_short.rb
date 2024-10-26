b95891051cab5edb14df838083740699bb9dc766
txyge <slastenov2004@mail.ru>
2024-10-26 10:02:20 +0300

    lab_2_task_2.4/5 add student_short

---
+++ Core classes and models/student_short.rb
@@ -0,0 +1,17 @@
+require_relative 'C:\Users\slast\OneDrive\Рабочий стол\Ruby\Core classes and models\base.rb'
+class Student_short < Base
+  def initialize(student, contact: nil)
+    super(id: student.id, git: student.git)
+    @surname_initials = "#{student.surname} #{student.first_name[0]}.#{student.last_name ? " #{student.last_name[0]}." : ''}"
+    @contact = student.get_contacts || contact
+  end
+
+  def to_s
+    "ID: #{@id}, ФИО: #{@surname_initials} Git: #{@git ? @git : 'нет'} Контакт: #{@contact ? @contact : 'нет'} "
+  end
+
+  def add_contact(contact)
+    @contact ||= []
+    @contact << "#{contact[:type]}: #{contact[:value]}" if contact
+  end
+end
