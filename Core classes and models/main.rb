d7151148e0d6caf62fdce176783aa477e329f908
txyge <slastenov2004@mail.ru>
2024-10-26 09:33:23 +0300

    lab_2_task_2.3 adding get_info

--- Core classes and models/main.rb
+++ Core classes and models/main.rb
@@ -1,29 +1,50 @@
 require_relative 'C:\Users\slast\OneDrive\Рабочий стол\Ruby\Core classes and models\student.rb'
-begin
-  student1 = Student.new("Иванов", "Иван", "Иванович", "1234567890", "@kkkk", "ikkk@pika.com", "github.com/ivan")
-  puts student1
-  student2 = Student.new("петр", "петров")
-  puts student2
-  student3 = Student.new("зубенко", "михаил", "петрович", "24626345634", )
-  puts student3
-rescue ArgumentError => e
-    puts e.message
-end
-begin
-    student1 = Student.parsing("Иванов, Иван, Иванович, +1 234-567-8901, @ivan, ivanov@example.com, https://github.com/ivan/repo.git")
-    puts student1
-  
-    student2 = Student.parsing("Петров, Петр")  # Это вызовет ошибку при валидации
-    puts student2
-  rescue ArgumentError => e
-    puts e.message
-  end
-  
-  begin
-    student3 = Student.parsing("зубенко, михаил, петрович, 24626345634")
-    puts student3
-    # Это вызовет ошибку, так как отсутствует Git URL
-  rescue ArgumentError => e
-    puts e.message
-  end
-  
+student1 = Student.new(
+  surname: 'Иванов',
+  first_name: 'Иван',
+  last_name: 'Иванович',
+  id: "1",
+  telegram: '@vinchik',
+  git: "https://github.com/vinchik/kashka.git"
+)
+
+student2 = Student.new(
+  surname: 'петров',
+  first_name: 'петр',
+  last_name: 'петрович',
+  id: "2",
+  phone: '+79999999999',
+  email: 'Gk7cA@example.com'
+)
+
+student3 = Student.new(
+  surname: 'ivanov',
+  first_name: 'ivan',
+  last_name: 'ivanovich',
+  id: "3",
+  telegram: '@ivanov',
+  phone: '+79999999999',
+  email: 'petrov@example.com'
+)
+
+# Создаем несколько студентов
+students = [
+  Student.parsing('ID: 1, Фамилия: Иванов, Имя: Иван, Отчество: Иванович, Телефон: +79162345678, Телеграм: @ivanov, Почта: ivanov@gmail.com, Гит: https://github.com/ivanov/repo.git'),
+  Student.parsing('ID: 2, Фамилия: ivanov, Имя: ivan, Отчество: ivаnovich, Телефон: +98765432101, Телеграм: @ivanovik, Почта: neivanov@gmail.com, Гит: https://github.com/neivanov/repo.git'),
+  Student.parsing('ID: 3, Фамилия: сидоров, Имя: сидро, Отчество: sidoroвич, Телефон: +98234568795, Телеграм: @sdrochuk, Почта: sadro@gmail.com, Гит: https://github.com/sidro/repo.git'),
+]
+
+# Вывод информации о студентах
+puts student1.getinfo
+puts "\n"
+puts student2.getinfo
+puts "\n"
+puts student3.getinfo
+
+puts "Студент 1 валиден? #{student1.validate?}"
+puts "\n"
+puts "Студент 2 валиден? #{student2.validate?}"
+puts "\n"
+puts "Студент 3 валиден? #{student3.validate?}"
+
+# Создаем несколько студентов
