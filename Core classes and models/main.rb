29e8167c4ad4d23ee2fb94be7d423eb3dd2ded84
txyge <slastenov2004@mail.ru>
2024-10-26 09:06:39 +0300

    lab_2_task_2.2 adding parsing

--- Core classes and models/main.rb
+++ Core classes and models/main.rb
@@ -1,9 +1,29 @@
-require_relative 'student_class'
-
-student1 = Student.new("Иванов", "Иван", "Иванович", "1234567890", "@kkkk", "ikkk@pika.com", "github.com/ivan")
-student2 = Student.new("ivan", "ivan", "ivanovich", "0987654321", "@aaaa", "aaa@aaaaaaaa.com", "github.com/vanok")
-student3 = Student.new("зубенко", "михаил", "петрович", "24626345634", "@zmp", "roz@vii.com", "github.com/sidor")
-
-puts student1
-puts student2
-puts student3
+require_relative 'C:\Users\slast\OneDrive\Рабочий стол\Ruby\Core classes and models\student.rb'
+begin
+  student1 = Student.new("Иванов", "Иван", "Иванович", "1234567890", "@kkkk", "ikkk@pika.com", "github.com/ivan")
+  puts student1
+  student2 = Student.new("петр", "петров")
+  puts student2
+  student3 = Student.new("зубенко", "михаил", "петрович", "24626345634", )
+  puts student3
+rescue ArgumentError => e
+    puts e.message
+end
+begin
+    student1 = Student.parsing("Иванов, Иван, Иванович, +1 234-567-8901, @ivan, ivanov@example.com, https://github.com/ivan/repo.git")
+    puts student1
+  
+    student2 = Student.parsing("Петров, Петр")  # Это вызовет ошибку при валидации
+    puts student2
+  rescue ArgumentError => e
+    puts e.message
+  end
+  
+  begin
+    student3 = Student.parsing("зубенко, михаил, петрович, 24626345634")
+    puts student3
+    # Это вызовет ошибку, так как отсутствует Git URL
+  rescue ArgumentError => e
+    puts e.message
+  end
+  
