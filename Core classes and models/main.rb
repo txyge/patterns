da3859d908ba8a5cc8cef6f2cf2cb83e8a89c4aa
txyge <slastenov2004@mail.ru>
2024-10-26 05:34:08 +0300

    lab_2_task_2 add main

---
+++ Core classes and models/main.rb
@@ -0,0 +1,9 @@
+require_relative 'student_class'
+
+student1 = Student.new("Иванов", "Иван", "Иванович", "1234567890", "@kkkk", "ikkk@pika.com", "github.com/ivan")
+student2 = Student.new("ivan", "ivan", "ivanovich", "0987654321", "@aaaa", "aaa@aaaaaaaa.com", "github.com/vanok")
+student3 = Student.new("зубенко", "михаил", "петрович", "24626345634", "@zmp", "roz@vii.com", "github.com/sidor")
+
+puts student1
+puts student2
+puts student3
