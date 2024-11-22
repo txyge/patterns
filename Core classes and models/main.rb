cd52f0cb20df8d1f7f253477bf175e551834de75
txyge <slastenov2004@mail.ru>
2024-11-22 20:26:15 +0300

    lab_2_task_2.1/3 update main

--- Core classes and models/main.rb
+++ Core classes and models/main.rb
@@ -2,6 +2,7 @@ require_relative 'C:\Users\slast\OneDrive\Рабочий стол\Ruby\Core clas
 
 student1 = Student.new(last_name: "Иванов", first_name: "Иван",surname: "Сергеевич", phone: "+79991234567", telegram: "@ivanov", mail: "ivanov@example.com", git: "https://github.com/ivanov")
 student2 = Student.new(id: 2, last_name: "Петров", first_name: "Петр", surname: "Сергеевич", phone: "+71234567890", telegram: "@petrov", mail: "petrov@example.com", git: "https://github.com/petrov")
-
-puts student1.get_contacts
+student3 = Student.from_string("3, Иванов, Иван, Иванович, +7 (999) 123-45-67, @ivanov, ivanov@example.com,  https://github.com/ivanov")
+puts student1.get_info
 puts student2.to_s
+puts student3
