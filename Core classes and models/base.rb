77815d3624b7787ce7af439aa4a4ec220fe93f9b
txyge <slastenov2004@mail.ru>
2024-11-23 11:07:32 +0300

    lab_2_task_2 update base

--- Core classes and models/base.rb
+++ Core classes and models/base.rb
@@ -1,29 +1,47 @@
 class Base
   attr_reader :id, :git
 
-  def initialize(id:, git: nil)
-    self.id = id
+  def initialize(id: nil, git: nil)
+    self.id = id if !id.nil?
     self.git = git if !git.nil?
   end
-
-  protected 
-
- 
-  # сеттеры
-  def git=(git)
-    if git.match?(/\Ahttps:\/\/github\.com\/[-a-zA-Z0-9@:%_\+.~#=]+\/[-a-zA-Z0-9._~%]+\.git\z/)
-      @git = git
-    else
-      raise ArgumentError, "Неправильно введен git"
-    end
-  end
-
 
   def id=(id)
     if id.match?(/^\d+$/)
       @id= id
     else
       raise ArgumentError, "Неправильно введен id"
+    end
+  end
+
+  def valid?()
+    if has_contacts?() && has_git?()
+      true
+    else
+      false
+    end
+  end
+
+  def has_contacts?()
+    raise NotImplementedError, "Метод has_contacts? должен быть переопределен в дочернем классе"
+  end
+
+  def has_git?()
+    if @git==nil
+      false
+    else
+      true
+    end
+  end
+
+  protected 
+
+  # сеттеры
+  def git=(git)
+    if git.match?(/\Ahttps:\/\/github\.com\/[-a-zA-Z0-9@:%_\+.~#=]+\/[-a-zA-Z0-9._~%]+\.git\z/)
+      @git = git
+    else
+      raise ArgumentError, "Неправильно введен git"
     end
   end
 end
