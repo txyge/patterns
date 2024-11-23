0e7e79d05c1c3eedb7b8948f91b5ef9d2197430d
txyge <slastenov2004@mail.ru>
2024-11-23 11:07:15 +0300

    lab_2_task_2 update main

--- Core classes and models/main.rb
+++ Core classes and models/main.rb
@@ -1,8 +1,8 @@
-require_relative 'C:\Users\slast\OneDrive\Рабочий стол\Ruby\Core classes and models\student.rb'
+require_relative 'C:\Users\Extrafly\Desktop\kostia\student.rb'
 
-student1 = Student.new(last_name: "Иванов", first_name: "Иван",surname: "Сергеевич", phone: "+79991234567", telegram: "@ivanov", mail: "ivanov@example.com", git: "https://github.com/ivanov")
-student2 = Student.new(id: 2, last_name: "Петров", first_name: "Петр", surname: "Сергеевич", phone: "+71234567890", telegram: "@petrov", mail: "petrov@example.com", git: "https://github.com/petrov")
-student3 = Student.from_string("3, Иванов, Иван, Иванович, +7 (999) 123-45-67, @ivanov, ivanov@example.com,  https://github.com/ivanov")
+student1 = Student.new(last_name: "Иванов", first_name: "Иван",surname: "Сергеевич", phone: "+79991234567", telegram: "@ivanov", mail: "ivanov@example.com")
+student2 = Student.new(id: "2", last_name: "Петров", first_name: "Петр", surname: "Сергеевич", phone: "+71234567890", telegram: "@petrov", mail: "petrov@example.com", git: "https://github.com/Bagdad1/repo.git")
+student3 = Student.from_string("3, Иванов, Иван, Иванович, +7 (999) 123-45-67, @ivanov, ivanov@example.com,  https://github.com/Bagdad1/repo.git")
 puts student1.get_info
 puts student2.to_s
 puts student3
