90e4e81b9dbb2ec97d5fff9c6ad1da1ead98224a
txyge <slastenov2004@mail.ru>
2024-11-30 10:29:25 +0300

    lab_2_task_2 correction in main

--- Core classes and models/main.rb
+++ Core classes and models/main.rb
@@ -1,4 +1,4 @@
-require_relative 'C:\Users\Extrafly\Desktop\kostia\student.rb'
+require_relative 'C:\Users\slast\OneDrive\Рабочий стол\Ruby\Core classes and models\student.rb'
 
 student1 = Student.new(last_name: "Иванов", first_name: "Иван",surname: "Сергеевич", phone: "+79991234567", telegram: "@ivanov", mail: "ivanov@example.com")
 student2 = Student.new(id: "2", last_name: "Петров", first_name: "Петр", surname: "Сергеевич", phone: "+71234567890", telegram: "@petrov", mail: "petrov@example.com", git: "https://github.com/Bagdad1/repo.git")
